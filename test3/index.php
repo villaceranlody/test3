9116020b75891109e4558062d8614deb57871939
villaceranlody <146922763+villaceranlody@users.noreply.github.com>
2024-11-07 15:06:56 +0800

    Add files via upload

---
+++ test3/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My first Ubuntu Server PHP Deployment</title>
+</head>
+<body>
+     	<p>My first Ubuntu Server PHP Deployment</p>
+</body>
+	<div>
+		<label>Firstname:</label>
+		<input type="text" name="Firstname">
+	</div>
+
+	<div>
+		<label>Middle Name:</label>
+		<input type="text" name="middlename">
+	</div>
+
+    <div>
+		<label>Lastname:</label>
+		<input type="text" name="lastname">
+	</div>
+</html>
